5c647a77d5375c65e5fd2fe58802bb2f4cef5c9d
方啸宇 <hou@fangxiaoyudeMBP.mshome.net>
2018-09-23 21:37:36 +0800

    add quicksort

---
+++ app/src/main/java/com/example/hou/algorithminjava/QuickSort.java
@@ -0,0 +1,64 @@
+package com.example.hou.algorithminjava;
+
+/**
+ * Created by hou on 2018/9/23.
+ */
+
+public class QuickSort {
+    private QuickSort() {
+    }
+
+    private static class Holder {
+        private static QuickSort sInstance = new QuickSort();  //单例
+    }
+
+    public static QuickSort getInsatnce() {
+        return Holder.sInstance;
+    }
+
+    public static void quickSort(int[] arr, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) {  //如果开始的位置大于等于末尾位置 就退出
+            return;
+        }
+        int pivotIndex = partition(arr, startIndex, endIndex);   //选择一个位置分成两段
+
+        quickSort(arr, startIndex, pivotIndex - 1);//每次分两段进行递归索引
+        quickSort(arr, pivotIndex + 1, endIndex);
+    }
+
+    private static int partition(int[] arr, int startIndex, int endIndex) {
+        int pivot = arr[startIndex];
+        int left = startIndex;
+        int right = endIndex;
+        int index = startIndex;    //此为挖出来的
+
+        while (right >= left) {  //从右至左进行检索交换
+            while (right >= left) {
+                if (arr[right] < pivot) {
+                    arr[left] = arr[right];
+                    index = right;
+                    left++;
+                    break;
+                }
+                right--;
+
+            }
+
+            while (right >= left) {
+                if (arr[left] > pivot) {
+                    arr[right] = arr[left];
+                    index = left;
+                    right++;
+                    break;
+                }
+                left++;
+            }
+
+        }
+
+        arr[index] = pivot;
+        return index;
+
+    }
+
+}
