2bbfe37895abd0c6826eb8fa6c0c67a236b98fc1
方啸宇 <hou@fangxiaoyudeMBP.mshome.net>
2018-10-04 21:28:01 +0800

    pr

---
+++ app/src/main/java/com/example/hou/algorithminjava/sort/ShellSort.java
@@ -0,0 +1,28 @@
+package com.example.hou.algorithminjava.sort;
+
+
+import com.example.hou.algorithminjava.Example;
+
+/*
+ *
+ * 希尔排序
+ * */
+public class ShellSort {
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        int h = 1;
+        while (h<N/3){
+            h = 3*h +1;
+        }
+        while (h>= 1){
+            for (int i = h; i < N; i++) {
+                for (int j=i;j>=h&&Example.less(a[j],a[j-h])){
+                    Example.exch(a,j,j-h);
+                }
+            }
+            h /=3;
+        }
+    }
+
+}
